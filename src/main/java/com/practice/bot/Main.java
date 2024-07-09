de511f27c802e1709216c327d11c9f52cc4bde2b
g00dme <uchtile@mail.ru>
2024-07-09 17:43:22 +0400

    Полноценная связь с редмином

--- src/main/java/com/practice/bot/Main.java
+++ src/main/java/com/practice/bot/Main.java
@@ -1,7 +1,21 @@
 package com.practice.bot;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
+    static RedmineBot redmine = new RedmineBot("http://localhost:3000","d8d70e4791fad295441a647234b245234abd7cc2");
+
+    public static void repeat() {
+        System.out.println(new java.util.Date());
+        redmine.get_difference();
+        System.out.println("differences: "+redmine.diff);
+        System.out.println("old issues: "+ redmine.diff_old);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ScheduledExecutorService executorService;
+        executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.scheduleAtFixedRate(Main::repeat, 0, 1, TimeUnit.MINUTES);
     }
 }
---
+++ src/main/java/com/practice/bot/RedmineBot.java
@@ -0,0 +1,112 @@
+package com.practice.bot;
+
+import com.taskadapter.redmineapi.RedmineException;
+import com.taskadapter.redmineapi.RedmineManager;
+import com.taskadapter.redmineapi.RedmineManagerFactory;
+import com.taskadapter.redmineapi.bean.Issue;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class RedmineBot {
+    RedmineManager mgr;
+    String url;
+    String api;
+    ArrayList<ArrayList<Object>> old_all;
+    ArrayList<ArrayList<Object>> now_all;
+    ArrayList<ArrayList<Object>> diff;
+
+    ArrayList<ArrayList<Object>> old_issue=new ArrayList<>();
+    ArrayList<ArrayList<Object>> new_old_issue;
+    ArrayList<ArrayList<Object>> diff_old;
+
+
+
+    RedmineBot(String url, String api) {
+        this.url=url;
+        this.api=api;
+
+        this.mgr=create_connection();
+        this.old_all=get_all_issue();
+    }
+
+    RedmineManager create_connection() {
+        RedmineManager mgr = RedmineManagerFactory.createWithApiKey(url, api);
+        mgr.setObjectsPerPage(100);
+        return mgr;
+    }
+
+    ArrayList<ArrayList<Object>> get_all_issue () {
+        List<Issue> issues;
+        try {
+            issues = mgr.getIssueManager().getIssues("hello", null);
+        } catch (RedmineException e) {
+            throw new RuntimeException(e);
+        }
+        ArrayList<ArrayList<Object>> all = new ArrayList<ArrayList<Object>>();
+        for (Issue issue : issues) {
+            ArrayList<Object> singleList = new ArrayList<Object>();
+            singleList.add(issue.getSubject());
+            singleList.add(issue.getPriorityText());
+            singleList.add(issue.getAssigneeName());
+            singleList.add(get_email(issue.getAssigneeId()));
+            singleList.add(issue.getAuthorName());
+            singleList.add(issue.getAuthorId());
+            singleList.add(get_email(issue.getAuthorId()));
+            singleList.add(issue.getUpdatedOn());
+
+            all.add(singleList);
+        }
+
+        return  all;
+    }
+
+    public static long getDateDiff(Date date1, Date date2, TimeUnit timeUnit) {
+        long diffInMillies = date2.getTime() - date1.getTime();
+        return timeUnit.convert(diffInMillies,TimeUnit.MILLISECONDS);
+    }
+
+    void check_old () {
+        ArrayList<ArrayList<Object>> old = new ArrayList<>();
+        for (ArrayList<Object> issue :this.now_all)  {
+            if (7 <= getDateDiff((Date) issue.getLast(), new Date(), TimeUnit.MINUTES)) {
+                old.add(issue);
+            }
+        this.new_old_issue=old;
+        }
+    }
+
+    String get_email(Integer id)  {
+        String email = "";
+        if (id != null) {
+            try {
+                email=this.mgr.getUserManager().getUserById(id).getMail();
+            } catch (RedmineException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return email;
+    }
+
+    void get_difference () {
+        this.now_all=get_all_issue();
+        check_old();
+
+        ArrayList<ArrayList<Object>> diff_old=new ArrayList<>();
+        diff_old.addAll(this.new_old_issue);
+
+        diff_old.removeAll(this.old_issue);
+        this.diff_old=diff_old;
+        this.old_issue=new_old_issue;
+
+        ArrayList<ArrayList<Object>> diff=new ArrayList<>();
+
+        diff.addAll(this.now_all);
+
+        diff.removeAll(this.old_all);
+        this.old_all=this.now_all;
+        this.diff=diff;
+    }
+}
