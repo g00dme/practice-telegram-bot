3283ebc43a382493ce445d9004eca9a78eecdacd
g00dme <uchtile@mail.ru>
2024-07-11 20:03:39 +0400

    Полноценная связь с редмином

--- src/main/java/com/practice/bot/Bot.java
+++ src/main/java/com/practice/bot/Bot.java
@@ -6,14 +6,13 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
 import java.util.HashMap;
 import java.util.Map;
 
 
 public class Bot extends TelegramLongPollingBot {
     static Map<String, Long> map = new HashMap<>();
-    long chatIdAdmin = 5355357934L;
+
 
     private static final Logger LOG = LoggerFactory.getLogger(Bot.class);
 
@@ -29,22 +28,22 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        map.put("leonidchr@mail.ru", chatIdAdmin);
 
         if(update.hasMessage() && update.getMessage().hasText() ) {
             String messageText = update.getMessage().getText();
             long chatId = update.getMessage().getChatId();
             switch (messageText){
-                case "/start":{
-                    try {
-                        startCommandReceived(chatId,update.getMessage().getChat().getFirstName());
-                    } catch (TelegramApiException e) {
-                        LOG.error("Ошибка при попытке ответить пользователю на комаду /start");
+                case "/start":
+                    if (!map.containsValue(update.getMessage().getChatId())) {
+                        startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    } else {
+                        String text = "Ты тупой? Ты уже авторизован";
+                        try {
+                            sendMessage(chatId,text);
+                        } catch (TelegramApiException e) {
+                            LOG.error("Не удалось отправить сообщение пользователю");
+                        }
                     }
-                }
-                case "/help":{
-
-                }
                 default:{
                     if (update.getMessage().getText().startsWith("Моя почта")){
                         if (map.get(update.getMessage().getText().substring(10).trim())!=null){
@@ -64,19 +63,20 @@ public class Bot extends TelegramLongPollingBot {
                             }
                             System.out.println(map);
                         }
-
-                    }else {
-                        LOG.error("Чё написал?");
                     }
                 }
             }
         }
     }
 
-    private void startCommandReceived(long chatId, String name) throws TelegramApiException{
+    private void startCommandReceived(long chatId, String name){
 
-        String Answer = "Здравствуй, " + name + ",\nчтобы начать получать уведомления о задачах в Redmine, пройди регистрацию или авторизируйся, отправив сообщение обязательного вида: Моя почта FunnyMonkey@gmail.com";
-        sendMessage(chatId, Answer);
+        String answer = "Здравствуй, " + name + ",\nчтобы начать получать уведомления о задачах в Redmine, пройди регистрацию, отправив сообщение обязательного вида: Моя почта FunnyMonkey@gmail.com";
+        try {
+            sendMessage(chatId, answer);
+        } catch (TelegramApiException e) {
+            LOG.error("Ошибка при формировании ответа на команду /start");
+        }
 
         LOG.info("Ответ на команду /start");
     }
@@ -90,8 +90,8 @@ public class Bot extends TelegramLongPollingBot {
         } catch (TelegramApiException e) {
             LOG.error("Не получилось сформировать ответ пользователю");
         }
-
     }
+
     static Map<String, Long> getMap(){
         Map<String, Long> botmap = new HashMap<>();
         botmap.putAll(map);
--- src/main/java/com/practice/bot/IssueProcessing.java
+++ src/main/java/com/practice/bot/IssueProcessing.java
@@ -1,32 +1,60 @@
 package com.practice.bot;
 
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
+import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Map;
 
 public class IssueProcessing {
 
-//    static Bot bot = new Bot();
+    public static void issueProcessing(ArrayList<ArrayList<Object>> diff,ArrayList<ArrayList<Object>> old,ArrayList<ArrayList<Object>> close){
+        Bot bot = new Bot();
 
-    static void getDiffIssue(){
-        Map<String, Long> getmap = Bot.getMap();
-        ArrayList<ArrayList<Object>> diff = Liist.differences();
-        for(int i=0; i<diff.size();i++){
-//            try {
-//                Bot.sendMessage(getmap.get(diff.get(i).get(3)),"У вас есть обновлённые задачи");
-//            } catch (TelegramApiException e) {
-//                throw new RuntimeException(e);
-//            }
+        if(LocalTime.now().isAfter(LocalTime.parse("09:00")) && LocalTime.now().isBefore(LocalTime.parse("18:00"))) {
+            for (int i = 0; i < diff.size(); i++) {
+                try {
+                    if (Bot.map.get(diff.get(i).get(3)) != null) {
+                        System.out.println(diff.get(i).get(3)); //TODO можно убрать
 
+                        String text = " Задача была обновлена\n название: %s\n приоритет: %s\n ответственные за исполнение: %s,\n задачу зарегистрировал: %s\n дата последнего коментария: %s";
+                        String formatText = String.format(text, diff.get(i).get(0), diff.get(i).get(1), diff.get(i).get(2), diff.get(i).get(4), diff.get(i).get(8));
+                        bot.sendMessage(Bot.map.get(diff.get(i).get(3)), formatText);
+                    }
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         }
 
+        if(LocalTime.now().isAfter(LocalTime.parse("09:00")) && LocalTime.now().isBefore(LocalTime.parse("13:00"))) {
+            for (int i = 0; i < old.size(); i++) {
+                try {
+                    if (Bot.map.get(old.get(i).get(3)) != null) {
+                            System.out.println(old.get(i).get(3));  //TODO можно убрать
 
-    }
-    static void getCloseIssue(ArrayList<ArrayList<Object>> close){
-        Map<String, Long> getmap = Bot.getMap();
+                            String text = " У вас есть задача с просроченным комментарием\n название: %s\n дата последнего коментария: %s";
+                            String formatText = String.format(text, old.get(i).get(0), old.get(i).get(8));
+                            bot.sendMessage(Bot.map.get(old.get(i).get(3)), formatText);
+                    }
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
 
+        if(LocalTime.now().isAfter(LocalTime.parse("09:00")) && LocalTime.now().isBefore(LocalTime.parse("13:00"))) {
+            for (int i = 0; i < close.size(); i++) {
+                try {
+                    if (Bot.map.get(close.get(i).get(3)) != null) {
+                        System.out.println(close.get(i).get(3));  //TODO можно убрать
 
-
+                        String text = " Задача приближается к крайнему сроку\n название: %s\n приоритет: %s\n ответственные за исполнение: %s,\n задачу зарегистрировал: %s\n дата последнего коментария: %s";
+                        String formatText = String.format(text, close.get(i).get(0), close.get(i).get(1), close.get(i).get(2), close.get(i).get(4), close.get(i).get(8));
+                        bot.sendMessage(Bot.map.get(close.get(i).get(3)), formatText);
+                    }
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 }
